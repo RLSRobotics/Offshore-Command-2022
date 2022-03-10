63ad1aff759b6e8258c6e4583a89469a3e58953d
Zack Sima <simayance@gmail.com>
2022-03-10 15:59:04 -0800

    v220310
    test

--- Offshore-Command-2022.ino
+++ Offshore-Command-2022.ino
@@ -1,4 +1,13 @@
-void setup() {}
 
-void loop() {}
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+}
 
+void loop() {
+  if(Serial.available() >= 1) {
+    
+    digitalWrite(13, Serial.read());
+  }
+}
