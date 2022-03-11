c2a6a83a51705a1496f7c51b848106a3013e80c2
Zack Sima <simayance@gmail.com>
2022-03-10 17:20:23 -0800

    Update Offshore-Command-2022.ino

--- Offshore-Command-2022.ino
+++ Offshore-Command-2022.ino
@@ -1,11 +1,21 @@
+int light = 44;
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(light, OUTPUT);
+  digitalWrite(light, 1);
 }
 
 void loop() {
-  if(Serial.available() >= 1) {
-    digitalWrite(13, Serial.read());
+  
+  //digitalWrite(13, a);
+  if(Serial.available() > 0) {
+    digitalWrite(light, Serial.read());
+    //String data = Serial.readStringUntil('\n');
+//    if (data == "1") {
+//          digitalWrite(light, 1);
+//      } else {
+//            digitalWrite(light, 0);
+//        }
+    
   }
 }
