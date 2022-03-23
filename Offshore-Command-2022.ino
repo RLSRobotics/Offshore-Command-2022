10bf566002b473becee3c237b7499d8f6bbad573
Zack Sima <simayance@gmail.com>
2022-03-22 17:23:58 -0700

    v220322

--- Offshore-Command-2022.ino
+++ Offshore-Command-2022.ino
@@ -1,21 +1,33 @@
+//int light = 44;
+//void setup() {
+//  Serial.begin(9600);
+//  pinMode(light, OUTPUT);
+//  digitalWrite(light, 1);
+//}
+//
+//void loop() {
+// 
+//}
+
+
 int light = 44;
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(5);
   pinMode(light, OUTPUT);
-  digitalWrite(light, 1);
 }
 
 void loop() {
-  
-  //digitalWrite(13, a);
-  if(Serial.available() > 0) {
-    digitalWrite(light, Serial.read());
-    //String data = Serial.readStringUntil('\n');
-//    if (data == "1") {
-//          digitalWrite(light, 1);
-//      } else {
-//            digitalWrite(light, 0);
-//        }
+  while (!Serial.available());
+  int x = Serial.readString().toInt();
     
+  if (x == 2) {
+    Serial.write("received 2");
+    digitalWrite(light, 0);
+  } else {
+    Serial.write("received other");
+    digitalWrite(light, 1);
   }
+  delay(20);
+  
 }
