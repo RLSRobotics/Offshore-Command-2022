6904b3a95394dcb67871d0e305035a4ad56ae0f0
Zack Sima <simayance@gmail.com>
2022-04-14 16:56:40 -0700

    v220414

--- Offshore-Command-2022.ino
+++ Offshore-Command-2022.ino
@@ -1,15 +1,3 @@
-//int light = 44;
-//void setup() {
-//  Serial.begin(9600);
-//  pinMode(light, OUTPUT);
-//  digitalWrite(light, 1);
-//}
-//
-//void loop() {
-// 
-//}
-
-
 int light = 44;
 void setup() {
   Serial.begin(9600);
@@ -18,16 +6,20 @@ void setup() {
 }
 
 void loop() {
-  while (!Serial.available());
-  int x = Serial.readString().toInt();
-    
-  if (x == 2) {
-    Serial.write("received 2");
+  //set integer as the number of bytes expected to be sent through python
+  while (Serial.available() < 3);
+  int bytesToRead = Serial.available();
+  byte input[bytesToRead];
+  
+  for (int i = 0; i < bytesToRead; i++) {
+    input[i] = Serial.read();
+  }
+  if (input[2] == 0xf1) {
+    Serial.print("received 0");
     digitalWrite(light, 0);
   } else {
-    Serial.write("received other");
+    Serial.print("received not 0");
     digitalWrite(light, 1);
   }
   delay(20);
-  
 }
