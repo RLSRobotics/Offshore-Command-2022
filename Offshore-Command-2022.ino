7d2b09cc5d611a6a35e7c71347f275ea5f0f397d
Zack Sima <simayance@gmail.com>
2022-04-21 16:15:09 -0700

    v220421

--- Offshore-Command-2022.ino
+++ Offshore-Command-2022.ino
@@ -1,27 +1,38 @@
+#include <Servo.h>
+
+Servo servo0;
+#define servo0Pin 2
+
 int light = 44;
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(5);
   pinMode(light, OUTPUT);
+
+  servo0.attach(servo0Pin);
+  servo0.writeMicroseconds(1500);
 }
 
 void loop() {
   //note: expecting byte array of x elements from python, bytearray([a, b, c]) works in python
   //set integer as the number of bytes expected to be sent through python
-  while (Serial.available() < 1);
+  while (Serial.available() < 2);
   int bytesToRead = Serial.available();
   byte input[bytesToRead];
   
   for (int i = 0; i < bytesToRead; i++) {
     input[i] = Serial.read();
   }
+  
+  int motorPower0 = (input[1] * 4) + 1100;
+  
+  servo0.writeMicroseconds(motorPower0);
+  
   if (input[0] == 1) {
-    //Serial.print works even with string concat and can be picked up by python
-    Serial.print("received 1");
     digitalWrite(light, 1);
   } else {
-    Serial.print("received not 1");
     digitalWrite(light, 0);
   }
+  Serial.print("received");
   delay(20);
 }
